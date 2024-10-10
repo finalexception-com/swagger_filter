6c8472bd1d0398d413ac96b68cdfb7ff01e9ee50
Tribog <julix4212@gmail.com>
2024-10-10 18:07:18 -0500

    feat: The model "Client" was crated.

---
+++ models/Client.cs
@@ -0,0 +1,38 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product_manager.models;
+[Table("clients")]
+public class Client
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("full_name")]
+    [MaxLength(250)]
+    public string FullName { get; set; }
+
+    [Column("direction")]
+    [MaxLength(300)]
+    public string Direction { get; set; }
+
+    [Column("phone_number")]
+    [MaxLength(20)]
+    public string PhoneNumber { get; set; }
+
+
+    public Client(string fullName, string direction, string phoneNumber)
+    {
+        FullName = fullName;
+        Direction = direction;
+        PhoneNumber = phoneNumber;
+    }
+
+    public Client()
+    {
+        
+    }
+}
