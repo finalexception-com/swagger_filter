a6ca642f849bb2d3db7faa97f1c331f9a774565d
Tribog <julix4212@gmail.com>
2024-10-10 18:41:05 -0500

    feat: the migrations and the table on the database were created.

--- Migrations/20241010234021_CreateTableClients.Designer.cs
+++ Migrations/20241010234021_CreateTableClients.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Product_manager.data;
 
@@ -10,9 +11,11 @@ using Product_manager.data;
 namespace Product_manager.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241010234021_CreateTableClients")]
+    partial class CreateTableClients
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241010234021_CreateTableClients.cs
@@ -0,0 +1,321 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product_manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableClients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clients",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    full_name = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    direction = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    phone_number = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clients", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive", "Rogelio" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 2,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients", "Jesse" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 3,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive", "Gertrude" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 4,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive", "Alfredo" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 5,
+                columns: new[] { "description", "name" },
+                values: new object[] { "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart", "Mercedes" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 6,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J", "Cecilia" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 7,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The Football Is Good For Training And Recreational Purposes", "Sidney" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 8,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive", "Luz" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 9,
+                columns: new[] { "description", "name" },
+                values: new object[] { "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016", "Enrique" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 10,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J", "Jeffrey" });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "description", "name", "price", "stock" },
+                values: new object[] { "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive", "Licensed Fresh Tuna", 8.3800000000000008, 62 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 2,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 5, "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016", "Licensed Wooden Gloves", 247.34, 98 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 3,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 4, "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart", "Licensed Plastic Shoes", 379.93000000000001, 88 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 4,
+                columns: new[] { "category_id", "name", "price", "stock" },
+                values: new object[] { 3, "Unbranded Frozen Keyboard", 435.95999999999998, 83 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 5,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 5, "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016", "Sleek Wooden Chicken", 349.61000000000001, 6 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 6,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 2, "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart", "Intelligent Fresh Ball", 326.38, 5 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 7,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 3, "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients", "Licensed Rubber Mouse", 924.14999999999998, 84 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 8,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 2, "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients", "Practical Concrete Sausages", 607.79999999999995, 23 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 9,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 5, "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart", "Refined Soft Chips", 994.74000000000001, 89 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 10,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 2, "The Football Is Good For Training And Recreational Purposes", "Refined Frozen Bacon", 850.30999999999995, 79 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "clients");
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design", "Joanna" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 2,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality", "Clarence" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 3,
+                columns: new[] { "description", "name" },
+                values: new object[] { "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart", "Jerome" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 4,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients", "Alyssa" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 5,
+                columns: new[] { "description", "name" },
+                values: new object[] { "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support", "Harriet" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 6,
+                columns: new[] { "description", "name" },
+                values: new object[] { "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles", "Zachary" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 7,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J", "Travis" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 8,
+                columns: new[] { "description", "name" },
+                values: new object[] { "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J", "Ann" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 9,
+                columns: new[] { "description", "name" },
+                values: new object[] { "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart", "Carmen" });
+
+            migrationBuilder.UpdateData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 10,
+                columns: new[] { "description", "name" },
+                values: new object[] { "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart", "Randy" });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "description", "name", "price", "stock" },
+                values: new object[] { "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit", "Rustic Concrete Bike", 44.579999999999998, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 2,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 1, "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive", "Rustic Fresh Sausages", 312.67000000000002, 97 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 3,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 5, "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles", "Ergonomic Frozen Chair", 575.47000000000003, 70 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 4,
+                columns: new[] { "category_id", "name", "price", "stock" },
+                values: new object[] { 2, "Sleek Concrete Shirt", 599.09000000000003, 78 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 5,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 1, "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients", "Awesome Wooden Car", 656.87, 30 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 6,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 4, "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals", "Unbranded Steel Computer", 368.73000000000002, 65 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 7,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 4, "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016", "Ergonomic Soft Bacon", 816.07000000000005, 32 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 8,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 4, "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support", "Practical Frozen Table", 261.01999999999998, 78 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 9,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 1, "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016", "Intelligent Fresh Chicken", 854.97000000000003, 66 });
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "id",
+                keyValue: 10,
+                columns: new[] { "category_id", "description", "name", "price", "stock" },
+                values: new object[] { 1, "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart", "Practical Soft Pants", 462.38999999999999, 1 });
+        }
+    }
+}
